10b6e837e320869000698bbda48c0bc87c6393fc
joaozada66 <133677947+joaozada66@users.noreply.github.com>
2023-09-25 12:23:21 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -7,31 +7,31 @@ function tocaSomClap(){
     document.querySelector('#som_tecla_clap').play();
 document.querySelector('.tecla_clap').onclick = tocaSomClap;}
 
-function tocaSomClap(){
-    document.querySelector('#som_tecla_clap').play();
-document.querySelector('.tecla_clap').onclick = tocaSomClap;}
+function tocaSomTim(){
+    document.querySelector('#som_tecla_tim').play();
+document.querySelector('.tecla_tim').onclick = tocaSomTim;}
 
-function tocaSomClap(){
-    document.querySelector('#som_tecla_clap').play();
-document.querySelector('.tecla_clap').onclick = tocaSomClap;}
+function tocaSomPuff(){
+    document.querySelector('#som_tecla_puff').play();
+document.querySelector('.tecla_Puff').onclick = tocaSomPuff;}
 
-function tocaSomClap(){
-    document.querySelector('#som_tecla_clap').play();
-document.querySelector('.tecla_clap').onclick = tocaSomClap;}
+function tocaSomSplash(){
+    document.querySelector('#som_tecla_splash').play();
+document.querySelector('.tecla_Splash').onclick = tocaSomSplash;}
 
-function tocaSomClap(){
-    document.querySelector('#som_tecla_clap').play();
-document.querySelector('.tecla_clap').onclick = tocaSomClap;}
+function tocaSomToim(){
+    document.querySelector('#som_tecla_toim').play();
+document.querySelector('.tecla_Toim').onclick = tocaSomToim;}
 
-function tocaSomClap(){
-    document.querySelector('#som_tecla_clap').play();
-document.querySelector('.tecla_clap').onclick = tocaSomClap;}
+function tocaSomPsh(){
+    document.querySelector('#som_tecla_psh').play();
+document.querySelector('.tecla_Psh').onclick = tocaSomPsh;}
 
-function tocaSomClap(){
-    document.querySelector('#som_tecla_clap').play();
-document.querySelector('.tecla_clap').onclick = tocaSomClap;}
+function tocaSomTic(){
+    document.querySelector('#som_tecla_tic').play();
+document.querySelector('.tecla_Tic').onclick = tocaSomTic;}
 
-function tocaSomClap(){
-    document.querySelector('#som_tecla_clap').play();
-document.querySelector('.tecla_clap').onclick = tocaSomClap;}
+function tocaSomTom(){
+    document.querySelector('#som_tecla_tom').play();
+document.querySelector('.tecla_Tom').onclick = tocaSomTom;}
 
