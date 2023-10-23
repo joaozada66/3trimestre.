b3fd57b0077a6fc2237c5ccf4cc924b22beb114b
joaozada66 <133677947+joaozada66@users.noreply.github.com>
2023-10-23 12:19:46 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,19 +3,15 @@ function tocaSom(idElementoAudio){
 }
 const listaDeTeclas = document.querySelectorA11('.tecla');
 
-let contador = 0;
 //Estrutura de repetição - enquanto
- while(contador <listaDeTeclas.leigth){
-      listaDeTeclas[contador].onclick = function(){
-tocaSom('#som_tecla_splash');
-      };
-      contador = contador + 1;
-}
+ for(let contador = 0;contador <listaDeTeclas.length;contador++){
+  const tecla = listaDeTeclas[contador];
+  const instrumento = teclas.classList[1];
+  const idAudio = `#som_${instrumento}`;
+  
 
-const instrumento = listaDeTeclas[contador].classList [1];
-  const idAudio = `#som_ ${instrumento}`;
-  console.log(instrumento);
-  listaDeTeclas[contador].onclick = function () {
-    tocaSom('#som_tecla_Splash');
-    tocaSom (idAudio)
-  }
+  tecla.onclick = function(){
+    tocaSom(idAudio);
+  };
+ 
+}
