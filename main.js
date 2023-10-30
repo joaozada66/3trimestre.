904d04231fffd7e2295b307ef9b7a3f3ba42a8c7
joaozada66 <133677947+joaozada66@users.noreply.github.com>
2023-10-30 12:30:14 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,28 @@
 function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
-
-const listaDeTeclas = document.querySelectorA11('.tecla');
+  const elemento = document.querySelector(idElementoAudio).play();
+  if (elemento === null) {
+    alert('elemento não encontrado')}
 }
-
+const listaDeTeclas = document.querySelectorAll('.tecla');
 //Estrutura de repetição - enquanto
  for(let contador = 0; contador <listaDeTeclas.length; contador++){
   const tecla = listaDeTeclas[contador];
-  const instrumento = teclas.classList[1];
+  const instrumento = tecla.classList[1];
   const idAudio = `#som_${instrumento}`;
+  console.log(instrumento); 
   console.log(idAudio);
 
   tecla.onclick = function(){
     tocaSom(idAudio);
   };
   tecla.onkeydown = function(){
+    if(evento.code === 'Enter' || evento.code ==='Space'){
     tecla.classList.add('ativa');
-  }
+   }
+};
   tecla.onkeyup = function(){
     tecla.classList.remove('ativa');
-  }
+  };
+  console.log(instrumento);
 console.log(contador);
 }
